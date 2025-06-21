89b5fb5a509e9ea3c05bcdd05a5ea13f59a0e8c0
botij0 <javier.gonzalez.soldado@gmail.com>
2025-06-21 12:39:00 +0200

    feat: new color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#ff005d",
+                secondary: "#00fff7",
+                tertiary: "#7a00ff",
+                accentLime: "#c6ff00",
+                warning: "#ff3131",
+                dark: "#121212",
+            },
+        },
+    },
+    plugins: [],
+};
